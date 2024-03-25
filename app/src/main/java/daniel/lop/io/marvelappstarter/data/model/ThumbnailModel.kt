640e98507a182ecc5721be39d277be911a12d95b
raphaeloliveira <raphael.oliveira@e-unicred.com.br>
2024-03-25 16:45:27 -0300

    criando as models

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/data/model/ThumbnailModel.kt
@@ -0,0 +1,12 @@
+package daniel.lop.io.marvelappstarter.data.model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class ThumbnailModel(
+
+    @SerializedName("path")
+    val path: String,
+    @SerializedName("extension")
+    val extension: String
+): Serializable
