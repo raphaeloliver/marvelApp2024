368ec6f6280c660eabb275938a8ca38b91884c12
raphaeloliveira <raphael.oliveira@e-unicred.com.br>
2024-03-27 14:54:33 -0300

    Criando a camada de service

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,8 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val PUBLIC_KEY = "cbdf16c98ffa37158c02627e542b05b2"
+    const val PRIVATE_KEY = "ebec6dc165cfc66f7abd675c0b70e868f67b7dfc"
+}
